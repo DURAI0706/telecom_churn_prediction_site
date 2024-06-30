08f1ef4ab7fb7c2952240b414b5f29a0b9d8b317
R DURAISAMY <112633012+DURAI0706@users.noreply.github.com>
2024-06-30 14:29:41 +0530

    Add files via upload

--- churn_prediction/settings.py
+++ churn_prediction/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#8-219#g1c$+bzt69_(y28gn$3-=wpulb6^wg(&*s*h6v*i$um
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = []
 
 
 # Application definition
